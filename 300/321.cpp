29e92f6da223ccd724700ce581e111048c2cbf9d
tshipenchko <dev@zxc.sx>
2022-09-22 09:35:54 +0600

    Add solution

--- 300/321.cpp
+++ 300/321.cpp
@@ -1,25 +1,20 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 double calculate(double x) {
-    // FIXME
     double result = 1;
-
-    for (int i = 2; i <= 64; i+=2) {
-        result *= (x - i);
-    }
-    for (int i = 1; i <= 63; i+=2) {
-        result /= (x - i);
+    
+    for (int i = 2; i <= 64; i *= 2) {
+        result = result * (x - i) / (x - i + 1);
     }
     
     return result;
 }
 
-
 int main() {
     double x;
     cin >> x;
     cout << calculate(x);
-
+    
     return 0;
 }
