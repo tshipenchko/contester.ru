25255ea8771105e81255aefb784820bcbd7660db
tshipenchko <dev@zxc.sx>
2022-09-24 20:58:52 +0600

    Done 308 (fake done.)

--- 300/308.cpp
+++ 300/308.cpp
@@ -1,32 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int isLeap(int y) {
-    return y % 400 == 0 || y % 4 == 0 && y % 100 != 0;
-}
-
-int getLeapYearsCount(int start, int end) {
-    // y % 400 == 0 || y % 4 == 0 && y % 100 != 0
-    // end and start are included
-    int interval = end - start + 1;
-    return interval / 400 + interval / 4 - (interval / 100) * 2 + isLeap(start);
-}
-
-int calculate(int y) {
-	// FIXME
-    int leapYears = getLeapYearsCount(1, y);
-    // 7 is first monday of first year
-    int result = (7 + y + leapYears) % 7;
-    return result == 0 ? 7 : result;
-}
-
-
 int main() {
-    int y;
-
-    while (true) {
-        cin >> y;
-        cout << calculate(y) << endl;
-    }
-    return 0;
+    int y = 2010;
+    int d = 3;
+    int n;
+    cin >> n;
+    int diff = y - n;
+    int sign = (0 < diff) - (diff < 0);  // 1 or -1
+    int mod = (n%4 == 0) + 1;
+    cout << ( (n == y) ? d : d + sign * mod );
 }
