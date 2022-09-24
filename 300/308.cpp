4ab43d3ce522e10cc4a3b6e1fb8443d036e6d780
tshipenchko <dev@zxc.sx>
2022-09-24 22:18:31 +0600

    Done 308

--- 300/308.cpp
+++ 300/308.cpp
@@ -2,12 +2,8 @@
 using namespace std;
 
 int main() {
-    int y = 2010;
-    int d = 3;
     int n;
     cin >> n;
-    int diff = y - n;
-    int sign = (0 < diff) - (diff < 0);  // 1 or -1
-    int mod = (n%4 == 0) + 1;
-    cout << ( (n == y) ? d : d + sign * mod );
+    cout << 7 - (int(365.25 * n) - 93) % 7;
+    return 0;
 }
