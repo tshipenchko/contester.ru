e87ddc1c30e38637c4b1f7f056aff82dad8c1dc5
tshipenchko <dev@zxc.sx>
2022-09-24 21:52:11 +0600

    Done 323

--- 300/323.cpp
+++ 300/323.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 double calculate(double x) {
-    // FIXME
     double result = 0;
+    double x2 = x*x;
 
     for (int i = 256; i > 1; i/=2) {
-        result = i/2./(x*x + i/(x*x));
+        result = i/(x2 + result);
     }
-    result *= 2;
 
-    return result;
+    return x/(x2 + result);
 }
-
 
 int main() {
     double x;
