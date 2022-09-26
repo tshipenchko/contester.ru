a29a71be59d8d4947090f6da2a88be12b622130a
tshipenchko <dev@zxc.sx>
2022-09-26 16:15:26 +0600

    Better done

--- 300/319.cpp
+++ 300/319.cpp
@@ -2,19 +2,14 @@
 #include <cmath>
 using namespace std;
 
-double factorial(double n) {
-    double f = 1;
-    for (int i=1; i<=n; ++i)
-        f *= i;
-    return f;
-}
-
 double calculate(double a) {
     double result = a;
+    double f = 1;
     int modifier = -1;
 
     for (int i = 3; i <= 13; i += 2) {
-        result += modifier * (pow(a, i) / factorial(i)); // was bug: double / int = int
+        f *= i*(i-1);
+        result += modifier * (pow(a, i) / f);
         modifier *= -1;
     }
 
