98e45a79c754f104239abbce9eab9182b8122292
tshipenchko <dev@zxc.sx>
2022-09-30 18:20:52 +0600

    Done 409

---
+++ 400/409.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[][4], int n) {
+    for (int i = 0, k = n-1; i < k; ++i) {
+        for (int j = 0, m = n-i-1; j < m; ++j) {
+            if (arr[j][3] > arr[j + 1][3]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n][4];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cin >> arr[i][j];
+        }
+        arr[i][3] = arr[i][0] * 3600 + arr[i][1] * 60 + arr[i][2];
+    }
+
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << arr[i][j] << ' ';
+        } cout << endl;
+    }
+
+    return 0;
+}
---
+++ 400/409_2.cpp
@@ -0,0 +1,38 @@
+// second way to solve
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[][3], int n) {
+    for (int i = 0, k = n-1; i < k; ++i) {
+        for (int j = 0, m = n-i-1; j < m; ++j) {
+            int j1 = j + 1;
+            if (arr[j][0] > arr[j1][0] || // hours >
+                arr[j][0] == arr[j1][0] && (arr[j][1] > arr[j1][1] || // hours = & minutes >
+                arr[j][0] == arr[j1][0] && arr[j][1] == arr[j1][1] && (arr[j][2] > arr[j1][2])) // hours = & minutes = & seconds >
+            ) swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n][3];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << arr[i][j] << ' ';
+        } cout << endl;
+    }
+
+    return 0;
+}
