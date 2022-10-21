20917ebf97cb0c6cf30469823ff714db9deea62a
tshipenchko <dev@zxc.sx>
2022-10-21 20:31:46 +0600

    Done 519 with step-by-step method

--- 500/519.cpp
+++ 500/519.cpp
@@ -1,52 +1,55 @@
 #include <iostream>
 using namespace std;
 
+bool isExist(int, int, int, int);
+
+
 int main() {
     int n;
     cin >> n;
-    bool arr[n][n];
+    int m = n;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            arr[i][j] = false;
+    bool arr[n][m];
+
+    bool* ptr = &arr[0][0];
+    while (ptr <= &arr[n-1][m-1]) *ptr++ = false;
+
+    int h = 1; int x = -1;
+    int v = 0; int y = 0;
+    for (int i = 0, l = n*m; i < l; i++) {
+        bool canPlace = (
+                (isExist(x+h, y+v, m, n) && !arr[y+v][x+h]) &&
+                (!isExist(x+h*2, y+v*2, m, n) || !arr[y+v*2][x+h*2])
+        );
+        if (canPlace) {
+            x += h;
+            y += v;
+            arr[y][x] = true;
+        } else {
+            if (h == 1) {
+                h = 0;
+                v = 1;
+            } else if (v == 1) {
+                v = 0;
+                h = -1;
+            } else if (h == -1) {
+                h = 0;
+                v = -1;
+            } else if (v == -1) {
+                v = 0;
+                h = 1;
+            }
         }
     }
     for (int i = 0; i < n; ++i) {
-        arr[0][i] = true;
-        arr[n-1][i] = true;
-        arr[i][n-1] = true;
-    }
-    int h = n-2, w = n-2;
-    int x = n-1, y = 0;
-    while (true) {
-        for (int i = 0; i < h; ++i) arr[x-i][y] = true;
-
-        x -= --h;
-        --h;
-        if(h <= 0) break;
-        for (int i = 0; i < w; ++i) arr[x][y+i] = true;
-
-        y += --w;
-        --w;
-        if (w <= 0) break;
-        for (int i = 0; i < h; ++i) arr[x+i][y] = true;
-
-        x += --h;
-        --h;
-        if (h <= 0) break;
-        for (int i = 0; i < w; ++i) arr[x][y-i] = true;
-
-        y -= --w;
-        --w;
-        if(w <= 0) break;
-    }
-    arr[n/2][n/2] = false;
-
-    for(int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < m; ++j) {
             cout << arr[i][j];
         } cout << endl;
     }
 
     return 0;
+}
+
+bool isExist(int x, int y, int m, int n) {
+    return x > -1 && x < m && y > -1 && y < n;
 }
