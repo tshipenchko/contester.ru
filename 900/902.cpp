feb6e7351dfd645e0c7345a5ea6c262574e5dd06
tshipenchko <dev@zxc.sx>
2022-10-29 14:39:16 +0600

    Done 902

---
+++ 900/902.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#define N 1000
+using namespace std;
+
+void replace(char*, char*);
+
+int main() {
+    char *source = new char[N];
+    char *dest = new char[N];
+
+    cin.getline(source, N);
+    replace(source, dest);
+    cout << dest;
+
+    return 0;
+}
+
+void replace(char *source, char *dest) {
+    for (int i = 0, l = (int)strlen(source); i < l; ++i) {
+        *(dest + i) = *(source + i) == '!' ? '.' : *(source + i);
+    }
+}
