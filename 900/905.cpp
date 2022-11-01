2c2695b775016b3a7cac039e26797c84ccecdae0
tshipenchko <dev@zxc.sx>
2022-11-01 17:32:33 +0600

    Done 905

---
+++ 900/905.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+#define N 1001
+#define M 31
+using namespace std;
+
+void deleteRepeats(char *out, char *text);
+
+
+int main() {
+    char text[N];
+    cin.getline(text, N);
+
+    int l = (int)strlen(text);
+
+    char out[N];
+    deleteRepeats(out, text);
+    cout << out;
+
+    return 0;
+}
+
+void deleteRepeats(char *out, char *text) {
+    char delim[] = " ";
+    out[0] = '\0';
+
+    char *tokenPtr = strtok(text, delim);
+    while (tokenPtr != NULL) { // NOLINT(modernize-use-nullptr)
+        if (strstr(out, tokenPtr) == NULL) { // NOLINT(modernize-use-nullptr)
+            strcat(out, tokenPtr);
+            strcat(out, " ");
+        }
+        tokenPtr = strtok(NULL, delim); // NOLINT(modernize-use-nullptr)
+    }
+}
